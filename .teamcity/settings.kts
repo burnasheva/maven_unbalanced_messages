117b20b88b5f64adff2a75a177b4c87f91188e86
admin <admin@TeamCity>
2021-05-18 10:40:49 +0200

    TeamCity change in 'Project from scratch' project: Versioned settings configuration updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,9 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.notifications
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.MavenBuildStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.slackConnection
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -31,55 +28,26 @@ version = "2021.1"
 
 project {
 
-    buildType(RunTests)
-
-    features {
-        slackConnection {
-            id = "PROJECT_EXT_69"
-            displayName = "Slack Test"
-            botToken = "credentialsJSON:0f24ef40-f107-4c0b-8bcf-0ddec3c92b25"
-            clientId = "2280447103.1138299994726"
-            clientSecret = "credentialsJSON:6e14cfcf-ad9a-42d7-bc27-6cc0dbdade59"
-        }
-    }
+    buildType(ProjectFromScratch_RunTests)
 }
 
-object RunTests : BuildType({
+object ProjectFromScratch_RunTests : BuildType({
+    id("RunTests")
     name = "run tests"
-    paused = true
 
     vcs {
         root(DslContext.settingsRoot)
     }
 
     steps {
-        script {
-            name = "hello world"
-            enabled = false
-            scriptContent = "echo hello world"
-            param("teamcity.runAs.windowsIntegrityLlevel", "auto")
-            param("teamcity.runAs.loggingLevel", "off")
-        }
         maven {
             goals = "clean test"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
-            localRepoScope = MavenBuildStep.RepositoryScope.MAVEN_DEFAULT
-            param("teamcity.runAs.windowsIntegrityLlevel", "auto")
-            param("teamcity.runAs.loggingLevel", "off")
         }
     }
 
-    features {
-        notifications {
-            notifierSettings = slackNotifier {
-                connection = "PROJECT_EXT_69"
-                sendTo = "U0AKGLCJC"
-            }
-            buildStarted = true
-            buildFailedToStart = true
-            buildFailed = true
-            buildFinishedSuccessfully = true
-            buildProbablyHanging = true
+    triggers {
+        vcs {
         }
     }
 })
