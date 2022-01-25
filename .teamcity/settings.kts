2e9dd08527c65165d59b18257b166cba401d78ba
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2022-01-25 14:23:35 +0100

    add .teamcity

---
+++ .teamcity/settings.kts
@@ -0,0 +1,67 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2020.2"
+
+project {
+
+    vcsRoot(HttpsGithubComBurnashevaMavenUnbalancedMessagesGitRefsHeadsMaster)
+
+    buildType(BuildProject)
+}
+
+object BuildProject : BuildType({
+    name = "build project (new name)"
+
+    vcs {
+        root(HttpsGithubComBurnashevaMavenUnbalancedMessagesGitRefsHeadsMaster)
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
+
+object HttpsGithubComBurnashevaMavenUnbalancedMessagesGitRefsHeadsMaster : GitVcsRoot({
+    name = "https://github.com/burnasheva/maven_unbalanced_messages.git#refs/heads/master"
+    url = "https://github.com/burnasheva/maven_unbalanced_messages.git"
+    branch = "refs/heads/master"
+    branchSpec = "refs/heads/*"
+    authMethod = password {
+        userName = "burnasheva"
+        password = "credentialsJSON:129576e4-c3ae-4675-a9bf-aaf179c9e8d2"
+    }
+})
+
