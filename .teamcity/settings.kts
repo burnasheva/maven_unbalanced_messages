031f03b905ec7ab62cca2fa01e920533c63443be
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2023-06-30 17:06:40 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -14,7 +14,10 @@ template(), and subProject() methods respectively.
 
 To debug settings scripts in command-line, run the
 
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-p
+
+
+lugin:generate
 
 command and attach your debugger to the port 8000.
 
